cf9863067f9d469b44f2cce290bbe06c0ff294c4
gnzhen <mioyazhen1996@gmail.com>
2019-10-28 12:55:46 +0800

    done transition

--- src/Box.js
+++ src/Box.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Box.css';
-import { Fade, Zoom, Slide, Grow } from '@material-ui/core';
+import { Grow } from '@material-ui/core';
 
 class Box extends React.Component {
   constructor(props) {
